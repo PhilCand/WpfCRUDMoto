535ffd525c4fc99b266bc4b637d41b325e5537b1
Philippe Canduro <philippe.canduro@gmail.com>
2019-11-15 11:25:05 +0100

    BD test relatif

--- WpfCRUDMoto/DAL.cs
+++ WpfCRUDMoto/DAL.cs
@@ -10,13 +10,17 @@ namespace WpfCRUDMoto
 {
     class DAL
     {
-        private static SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString);
+        private static SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionTestString"].ConnectionString);
+
+        static DAL()
+        {
+            connection.Open();
+        }
 
         public static List<Personne> GetPersonMoto()
         {
             List<Personne> Personnes = new List<Personne>();
             string sqlQuery = String.Format("SELECT p.id AS personneID, p.nom, m.id AS motoID, m.marque, m.cylindree FROM personnes p left join motos m ON p.id = m.personnes_id;");
-            connection.Open();
             SqlCommand command = new SqlCommand(sqlQuery, connection);
             SqlDataReader dataReader = command.ExecuteReader();
             Personne newMotard = new Personne();
@@ -43,7 +47,8 @@ namespace WpfCRUDMoto
                     }
                 }
             }
-            connection.Close();
+            dataReader.Close();
+            command.Dispose();            
             return Personnes;
         }
 
@@ -51,29 +56,30 @@ namespace WpfCRUDMoto
         public static int CreatePerson(Personne newMotard)
         {
             string sqlQuery = ($"Insert into personnes (nom) Values('{newMotard.Nom}');" + "Select @@Identity");
-            connection.Open();
             SqlCommand command = new SqlCommand(sqlQuery, connection);
             int newMotardID = Convert.ToInt32((decimal)command.ExecuteScalar());
-            connection.Close();
+            command.Dispose();
             return newMotardID;
         }
 
         public static void UpdatePerson(Personne selectedPersonne)
         {
             string updateQuery = $"Update personnes SET nom = '{selectedPersonne.Nom}' Where id = '{selectedPersonne.Id}';";
-            connection.Open();
+
             SqlCommand command = new SqlCommand(updateQuery, connection);
             command.ExecuteNonQuery();
-            connection.Close();
+
+            command.Dispose();
         }
 
         public static void DeletePerson(int PersonID)
         {
             string sqlQuery = String.Format($"delete from personnes where id = {PersonID}");
-            connection.Open();
+
             SqlCommand command = new SqlCommand(sqlQuery, connection);
             int rowsDeletedCount = command.ExecuteNonQuery();
-            connection.Close();
+
+            command.Dispose();
         }
         #endregion
 
@@ -81,28 +87,31 @@ namespace WpfCRUDMoto
         public static void CreateMoto(Moto newMoto)
         {
             string sqlQuery = ($"Insert into motos (marque, cylindree, personnes_id) Values('{newMoto.Marque}', '{newMoto.Cylindree}', '{newMoto.IdProprietaire}');" + "Select @@Identity");
-            connection.Open();
+
             SqlCommand command = new SqlCommand(sqlQuery, connection);
             command.ExecuteNonQuery();
-            connection.Close();
+
+            command.Dispose();
         }
 
         public static void UpdateMoto(Moto selectedMoto)
         {
             string updateQuery = $"Update motos SET marque = '{selectedMoto.Marque}', cylindree = '{selectedMoto.Cylindree}' Where id = '{selectedMoto.Id}';";
-            connection.Open();
+
             SqlCommand command = new SqlCommand(updateQuery, connection);
             command.ExecuteNonQuery();
-            connection.Close();
+
+            command.Dispose();
         }
 
         public static void DeleteMoto(int MotoID)
         {
             string sqlQuery = String.Format($"delete from motos where id = {MotoID}");
-            connection.Open();
+
             SqlCommand command = new SqlCommand(sqlQuery, connection);
             int rowsDeletedCount = command.ExecuteNonQuery();
-            connection.Close();
+
+            command.Dispose();
         }
         #endregion
     }
